c8c15b6c63251c806fcc4f397fac3202fc1cb9af
qyh <1649026770@qq.com>
2023-10-27 22:49:27 +0800

    完成rust课程第二章

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
     println!("猜数");
 
+    let secret_number = rand::thread_rng().gen_range(1..=101);
     println!("猜测一个数");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-
-    println!("你猜测的数是: {}", guess);
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+    
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("你猜测的数是: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
